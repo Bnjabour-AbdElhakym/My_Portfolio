b9d0a36c16db04c0e18f81f8df2f0ff53ca7bd3a
Bnjabour Abd-Elhakym <areyoubnjabour@gmail.com>
2023-10-29 22:37:14 +0100

    finich my portifolio and add it to Github in case i want to update it @Bnjabour-AbdElhakym

---
+++ public/main.js
@@ -0,0 +1,15 @@
+
+ScrollReveal({ reset: true, distance: "60px", duration: 2500, delay: 400 });
+
+// JavaScript
+ScrollReveal().reveal('.master-title, .second-title, .theard-title', { delay: 500, origin: 'left' });
+ScrollReveal().reveal(".main-img", { delay: 600, origin: 'bottom', opacity: "85px", scale: .9 });
+ScrollReveal().reveal(".p-info", { delay: 700, origin: 'right' });
+ScrollReveal().reveal(".media-icons i", { delay: 500, origin: "bottom", interval: 180 });
+
+ScrollReveal().reveal(".sec-img", { delay: 600, origin: "top" });
+ScrollReveal().reveal(".motivation", { delay: 600, origin: "right" });
+ScrollReveal().reveal(".more", { delay: 700, origin: "bottom" });
+
+ScrollReveal().reveal(".thrd-img", { delay: 500, origin: "top" });
+ScrollReveal().reveal(".social-media a", { delay: 500, origin: "left", interval: 200 });
